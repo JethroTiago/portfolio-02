a94765831c70b580d6eaffa858cadb60e8605b24
Jethro <jethrotiago@hotmail.com>
2022-12-09 22:23:06 -0300

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro"
+
 function App() {
   return (
     <div className="app">
       <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
