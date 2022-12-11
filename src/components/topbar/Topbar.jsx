631107e2db8fcd6031ca65cc7ae00e7b6846579a
Jethro <jethrotiago@hotmail.com>
2022-12-10 22:37:58 -0300

    Starting navabar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,13 @@ import "./topbar.scss"
 export default function topbar() {
   return (
     <div className="topbar">
-
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">teste</a>
+        </div>
+        <h1>JETHRO</h1>
+        <div className="right"></div>
+      </div>
     </div>
   )
 }
