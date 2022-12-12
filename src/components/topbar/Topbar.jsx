f6fc3c5cc54df2df91e288080a1136eaac6d44c8
Jethro <jethrotiago@hotmail.com>
2022-12-11 22:15:25 -0300

    Styles in navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -8,7 +8,12 @@ export default function topbar() {
         <div className="left">
           <a href="#intro" className="logo">jethro</a>
           <div className="itemContainer">
-
+            <Person className="icon" />
+            <span>+55 71 99296-9091</span>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>jethrotiago@hotmail.com</span>
+          </div>
           </div>
         </div>
         <div className="right"></div>
