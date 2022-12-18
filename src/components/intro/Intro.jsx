464d8235ac0eece072da5511bba7addf8b6c3af4
Jethro <jethrotiago@hotmail.com>
2022-12-17 22:59:52 -0300

    Added img

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,11 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      <div className="left"></div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/Jethro-Avatar.jpg" alt="Imagem do Jethro" />
+        </div>
+      </div>
       <div className="right"></div>
     </div>
   )
