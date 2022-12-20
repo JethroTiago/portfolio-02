0796cf178906e62e8a47010fe94a0f0c77e85ce0
Jethro <jethrotiago@hotmail.com>
2022-12-19 23:02:06 -0300

    Starting portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,6 +5,19 @@ export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
+      <ul>
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>SASS</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   )
 }
