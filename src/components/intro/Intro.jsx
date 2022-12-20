5e25bf548f9f74ec2f98565a66b9d79a45570ba7
Jethro <jethrotiago@hotmail.com>
2022-12-19 22:57:40 -0300

    Ending type animation with ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./intro.scss";
-import { init } from 'ityped'
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Front-End'] })
+  },[]);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +26,7 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Olá, galera!</h2>
           <h1>Jethro Tiago</h1>
-          <h3>Desenvolvedor <span>Front-End</span></h3>
+          <h3>Desenvolvedor <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="Seta pra baixo" />
