ff778e6aa725c628bd457e355ed6cd582e0993d7
Jethro <jethrotiago@hotmail.com>
2022-12-21 23:16:58 -0300

    Adding dynamic active class

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
 
 export default function Portfolio() {
+  const [selected,setSelected] = useState("featured");
+  const list = [
+    {
+      id: "react",
+      title: "React",
+    },
+    {
+      id: "html",
+      title: "HTML",
+    },
+    {
+      id: "css",
+      title: "CSS",
+    },
+    {
+      id: "bootstrap",
+      title: "Bootstrap",
+    },
+    {
+      id: "sass",
+      title: "Sass",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">React</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Bootstrap</li>
-        <li>SASS</li>
+        {list.map(item=>(
+          <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
